dd8311a2d3bd954117728a50b66910ab903fce38
Basil Udoudoh <basilu@slalom.com>
2019-06-16 13:39:17 -0400

    Readding keys file

---
+++ keys.js
@@ -0,0 +1,15 @@
+const apiKey = "Google Search API Key";
+const cx = "Google search CX";
+const groupMeToken = "GroupMe API Key";
+const botId = "GroupMe Bot ID ";
+const wolframId = "Wolfram Alpha API Key";
+const alphavantage = "Alpha Vantage API Key";
+
+module.exports = {
+    groupMeToken: groupMeToken,
+    botId: botId,
+    apiKey: apiKey,
+    cx: cx,
+    wolframId: wolframId, 
+    alphavantage: alphavantage
+}
